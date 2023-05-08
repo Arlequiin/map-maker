7f0f6d0289d0c51604b4960de50f351b04a72157
Arlequiin <78297845+Arlequiin@users.noreply.github.com>
2023-05-08 17:48:10 +0100

    resize end

--- static/editor/script.js
+++ static/editor/script.js
@@ -64,3 +64,27 @@ function updateGrid() {
 
 //faire un fetch, un get answer pour regexer l'html et renvoyer une matrice de type [A51, A10...]
 
+function getTileprompt() {
+  let coord = prompt("Tile selection (eg. 2,3)");
+  const image = document.getElementById("tileset");
+  const coordinates = document.getElementById("coordinate");
+  let dropdown = document.getElementById('drop').value
+  function handleMouseMove(event) {
+      const x = event.offsetX;
+      const y = event.offsetY;
+      fetch(`/selection/${x}/${y}/${dropdown}/${coord}`)
+      .then(response => response.json())
+      .then(data => {
+          let style_rect = `width: 16px; height: 16px; object-position: -${data.box[0]}px -${data.box[1]}px; object-fit: none;`;
+          const img = document.getElementById("cropped-image");
+          img.src = `static/data/tilesets/images/${dropdown}`;
+          img.style=style_rect;
+          img.classList = []; 
+          img.classList.add(`${data.box[0]}|${data.box[1]}`)
+          //img.classList.add(`width: 16px; height: 16px; background-position: -${data.box[0]}px -${data.box[1]}px; background-image:url(${img.src})`)
+      })
+      .catch(error => console.error(error));
+      image.removeEventListener("mousemove", handleMouseMove);
+  }
+  image.addEventListener("mousemove", handleMouseMove); 
+}
